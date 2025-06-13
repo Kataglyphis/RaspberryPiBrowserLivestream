9f844932b1a685df655a5b6afdf0d9fa4fff6a3b
Jonas Heinle <jonas.heinle@ipa.fraunhofer.de>
2025-06-13 20:21:53 +0200

    test fixes

--- tests/integration/test_dummy.py
+++ tests/integration/test_dummy.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from kataglyphispythonpackage.dummy import SimpleMLPreprocessor
+from raspberrypibrowserlivestream.dummy import SimpleMLPreprocessor
 
 
 def test_pipeline(monkeypatch):
